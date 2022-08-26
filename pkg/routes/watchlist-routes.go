24d878cf46aa3b1fccdb8d80d4bb535312fc15fc
0xMarvell <rokemarvellous@gmail.com>
2022-08-26 17:37:50 +0100

    added routes for API endpoints

--- pkg/routes/watchlist-routes.go
+++ pkg/routes/watchlist-routes.go
@@ -5,6 +5,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// RegisterRoutes handles the routing for the API endpoints.
 func RegisterRoutes(r *mux.Router) {
 	r.HandleFunc("/", controllers.Home).Methods("GET")
+	r.HandleFunc("/watchlist", controllers.ViewWatchlist).Methods("GET")
+	r.HandleFunc("/watchlist/add", controllers.AddShow).Methods("POST")
+	r.HandleFunc("/watchlist/{ShowId}", controllers.GetShowById).Methods("GET")
+	r.HandleFunc("/watchlist/{ShowId}", controllers.UpdateShow).Methods("PUT")
+	r.HandleFunc("/watchlist/{ShowId}", controllers.DeleteShow).Methods("DELETE")
 }
