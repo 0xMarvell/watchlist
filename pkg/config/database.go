ae27b4054b8f2603a17690d6e968b6915d7d0a55
0xMarvell <rokemarvellous@gmail.com>
2022-09-01 19:58:27 +0100

    added func to handle database connection

---
+++ pkg/config/database.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func Connect() {
+	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	d, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
