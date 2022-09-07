8278ef3a344c7cd6cf74f4c16448b740df499f6e
0xMarvell <rokemarvellous@gmail.com>
2022-09-07 15:14:28 +0100

    added init function for shows models

--- pkg/models/shows.go
+++ pkg/models/shows.go
@@ -1,9 +1,25 @@
 package models
 
+import (
+	"github.com/0xMarvell/watchlist/pkg/config"
+	"github.com/0xMarvell/watchlist/pkg/utils"
+	"gorm.io/gorm"
+)
+
+var Db *gorm.DB
+
 // Show is the database model for storing tv shows or movies.
 type Show struct {
 	Title       string `json:"name"`
 	Type        string `json:"type"`
 	ReleaseDate string `json:"release_date"`
 	Seen        bool   `json:"seen"`
+}
+
+func init() {
+	config.Connect()
+	Db = config.GetDB()
+	err := Db.AutoMigrate(&Show{})
+	utils.HandleErr("Migration failed:", err)
+
 }
