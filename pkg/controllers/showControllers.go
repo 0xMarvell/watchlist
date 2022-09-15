d1f9b84226b71fbd6eb6232e7c79da41fea8478b
0xMarvell <rokemarvellous@gmail.com>
2022-09-15 11:55:04 +0100

    created Show db model

--- pkg/controllers/showControllers.go
+++ pkg/controllers/showControllers.go
@@ -1,1 +1,10 @@
 package controllers
+
+import "gorm.io/gorm"
+
+type Show struct {
+	gorm.Model
+	Name      string `json:"name"`
+	Category  string `json:"category"`
+	Completed bool   `json:"completed"`
+}
