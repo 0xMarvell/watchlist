6a5f080e1675a94891a353219de446f2b1a8cfc0
0xMarvell <rokemarvellous@gmail.com>
2022-09-20 12:15:11 +0100

    fixed error from request body

--- pkg/controllers/userControllers.go
+++ pkg/controllers/userControllers.go
@@ -11,11 +11,17 @@ import (
 
 // Signup allows a user to register new account with the expected user details
 func Signup(c *gin.Context) {
-	// Get data off req body
+	// Get data off req body and bind it
 	var signupPayload struct {
 		Name     string `json:"name"`
 		Email    string `json:"email"`
 		Password string `json:"password"`
+	}
+	if c.Bind(&signupPayload) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "failed to read request body",
+		})
+		return
 	}
 	// Hash password
 	pwdHash, err := bcrypt.GenerateFromPassword([]byte(signupPayload.Password), 10)
@@ -23,6 +29,7 @@ func Signup(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to hash password",
 		})
+		return
 	}
 	// Store details in DB
 	user := models.User{
@@ -35,6 +42,7 @@ func Signup(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to create user",
 		})
+		return
 	}
 	// Return JSON response to confirm operation success
 	c.JSON(http.StatusCreated, gin.H{
